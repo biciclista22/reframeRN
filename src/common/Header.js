74c345a5fe6523891c167a13886a7850e5af20f4
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2018-01-02 18:46:08 -0800

    Change header color

--- src/common/Header.js
+++ src/common/Header.js
@@ -19,12 +19,12 @@ const Header = (props) => {
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8',
+    backgroundColor: '#00e4ff',
     alignItems: 'center',
     justifyContent: 'center',
     height: 60,
     paddingTop: 15,
-    shadowColor: 'black',
+    shadowColor: 'purple',
     shadowOffset: { width: 0, height: 2},
     shadowOpacity: 0.3,
     elevation: 2,
