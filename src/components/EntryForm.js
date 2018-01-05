4067268e1cfc50e7973fedffa4d9fce976ed93f1
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2018-01-05 13:54:36 -0800

    Functioning post request to rails API for new entry

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -1,29 +1,63 @@
 import React, { Component } from 'react';
 import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
+import axios from 'axios';
+
 
 class EntryForm extends Component {
   state = { text: ''};
 
+  postEntry() {
+    // console.log(this.state.text)
+    axios({
+      method: 'post',
+      url: 'http://localhost:3000/entries',
+      data: {
+        entry: {text: this.state.text}
+      }
+    })
+  }
+
+  //   axios.post('localhost:3000/entries', {
+  //     entry: {text: this.state.text}})
+  //     .then(response => (console.log(response)));
+  //   // a request will return a promise back to us... it is asynchronus
+  // }
+
+
+  //   fetch('https://localhost:3000/entries', {
+  //     method: 'POST',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       entry: {
+  //         text: this.state.text
+  //       }
+  //     }),
+  //   });
   render() {
     return (
       <Card>
 
       <CardSection>
-        <TextInput
-        value={this.state.text}
-        onChangeText={text => this.setState({text})}
-        style={{ height: 100, width: 350}}
-        multiline={true}
-        autoCorrect={true}
-        placeholder='Start reframing...'
-        />
+      <TextInput
+      value={this.state.text}
+      onChangeText={text => this.setState({text})}
+      style={{ height: 100, width: 350}}
+      multiline={true}
+      autoCorrect={true}
+      // lineHeight: 23
+      // fontSize: 18
+      placeholder='Start reframing...'
+      />
       </CardSection>
 
       <CardSection>
-        <Button>
-          Log it!
-        </Button>
+      <Button onPress={this.postEntry.bind(this)} >
+      Log it!
+      </Button>
       </CardSection>
 
       </Card>
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
 const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
