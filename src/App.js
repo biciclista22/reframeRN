fb6cf158971e5dcca2fb1f75a058c2fdde750517
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2018-01-08 11:45:26 -0800

    Flesh out login form and imported it into app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Header } from './components/common';
 import EntryForm from './components/EntryForm';
-
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   render() {
     return (
       <View>
         <Header headerText="Reframe"/>
+        <LoginForm />
         <EntryForm />
       </View>
     );
