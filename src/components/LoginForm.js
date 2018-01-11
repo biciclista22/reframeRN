a2cedf28172a4a83500692e41d04b24d1343919c
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2018-01-11 14:25:57 -0800

    Got initial async working to store the user_id

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,33 +1,85 @@
 import React, { Component } from 'react';
+import { AsyncStorage } from 'react-native';
 import { Button, Card, CardSection, Input, Spinner } from './common';
 import axios from 'axios';
+
+const USER_ID = 'userId';
+
 
 class LoginForm extends Component {
 
   state = { email: '', password: '', error: '', loading: false};
 
-
-
-  createUser() {
-
-    const { email, password } = this.state;
-
-    this.setState({error: '', loading: true });
-
-    axios({
-      method: 'post',
-      url: 'http://localhost:3000/users',
-      data: {
-        user: {name: this.state.email, email: this.state.email, password: this.state.password}
-      }
-    // }).catch( () => {
-      // need to add conditional statement to check if it doesn't already exist, create user
-      // also want to chain a .then(this.onLoginSuccess.bind(this))
-    // }).catch( () => {
-      // need to show error messaging if the email exists but the password doesn't
-      // here can instead call .catch(this.onLoginFail.bind(this));
-    }).then(this.onLoginSuccess.bind(this))
+  async storeUserId(userId) {
+    userIdString = JSON.stringify(userId)
+    // console.log("ID was stored successfully");
+    try {
+      AsyncStorage.setItem(USER_ID, userIdString)
+      console.log("ID was stored successfully");
+    } catch(error) {
+      console.log("Something went wrong");
+    }
   }
+
+  async onRegisterPressed() {
+    try {
+      let response = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user:{
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+          }
+        })
+      });
+
+      let res = await response.text();
+
+      //Handle success
+      if (response.status >= 200 && response.status < 300) {
+        var json = JSON.parse(response._bodyText);
+        let userId = json.user_id;
+
+        //On success we will store the access_token in the AsyncStorage
+        this.storeUserId(userId);
+        // this.redirect('home');
+      } else {
+        //Handle error
+        let error = res;
+        throw error;
+      }
+
+    } catch(errors) {
+
+    }
+
+  }
+
+  // createUser() {
+  //
+  //   const { email, password, user_id } = this.state;
+  //
+  //   this.setState({error: '', user_id: '' loading: true });
+  //
+  //   axios({
+  //     method: 'post',
+  //     url: 'http://localhost:3000/users',
+  //     data: {
+  //       user: {name: this.state.email, email: this.state.email, user_id: this.state.user_id}
+  //     }
+  //     // }).catch( () => {
+  //     // need to add conditional statement to check if it doesn't already exist, create user
+  //     // also want to chain a .then(this.onLoginSuccess.bind(this))
+  //     // }).catch( () => {
+  //     // need to show error messaging if the email exists but the password doesn't
+  //     // here can instead call .catch(this.onLoginFail.bind(this));
+  //   }).then(this.onLoginSuccess.bind(this))
+  // }
 
   onLoginSuccess() {
     this.setState({
@@ -51,8 +103,8 @@ class LoginForm extends Component {
     }
 
     return (
-      <Button onPress={this.createUser.bind(this)}>
-        Create Account
+      <Button onPress={this.onRegisterPressed.bind(this)}>
+      Create Account
       </Button>
     )
   }
@@ -60,28 +112,28 @@ class LoginForm extends Component {
   render() {
     return (
       <Card>
-        <CardSection>
-          <Input
-            placeholder="user@gmail.com"
-            label="Email"
-            value={this.state.email}
-            onChangeText={email => this.setState({ email })}
-          />
-        </CardSection>
+      <CardSection>
+      <Input
+      placeholder="user@gmail.com"
+      label="Email"
+      value={this.state.email}
+      onChangeText={email => this.setState({ email })}
+      />
+      </CardSection>
 
-        <CardSection>
-          <Input
-            secureTextEntry={true}
-            placeholder="password"
-            label="Password"
-            value={this.state.password}
-            onChangeText={password => this.setState({ password })}
-          />
-        </CardSection>
+      <CardSection>
+      <Input
+      secureTextEntry={true}
+      placeholder="password"
+      label="Password"
+      value={this.state.password}
+      onChangeText={password => this.setState({ password })}
+      />
+      </CardSection>
 
-        <CardSection>
-          {this.renderButton()}
-        </CardSection>
+      <CardSection>
+      {this.renderButton()}
+      </CardSection>
       </Card>
     );
   }
