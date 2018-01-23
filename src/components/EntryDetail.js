7089c832c8577f4b57eba3c5f7606f173b9f37c3
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2018-01-23 15:33:34 -0800

    Minor improvements on styling of entries

--- src/components/EntryDetail.js
+++ src/components/EntryDetail.js
@@ -1,19 +1,58 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Card } from './common/Card';
 import { CardSection } from './common/CardSection';
+import { Icon } from 'react-native-elements'
 
 const EntryDetail = (props) => {
   return (
-    <Card>
-      <CardSection>
-        <Text>{props.entry.date}</Text>
-      </CardSection>
-      <CardSection>
-        <Text>{props.entry.text}</Text>
-      </CardSection>
-    </Card>
+    <View style={styles.viewStyle}>
+    <View style={styles.dateStyle}>
+      <Icon
+        name='toys' />
+      <Text style={{paddingLeft: 5}}>{props.entry.date}</Text>
+    </View>
+    <View style={styles.entryStyle}>
+      <Text>{props.entry.text}</Text>
+    </View>
+    </View>
   );
 };
 
+const styles = {
+  viewStyle: {
+    borderWidth: 1,
+    borderRadius: 2, // rounded edges
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: .1,
+    shadowRadius: 2, // at corners of the shadow, will round them out
+    elevation: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10,
+    marginBottom: 20
+  },
+  dateStyle: {
+    backgroundColor: '#86A0D4',
+    borderBottomWidth: 1,
+    padding: 10,
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#86A0D4',
+    position: 'relative',
+  },
+  entryStyle: {
+    borderBottomWidth: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#86A0D4',
+    position: 'relative',
+
+  }
+}
 export default EntryDetail;
